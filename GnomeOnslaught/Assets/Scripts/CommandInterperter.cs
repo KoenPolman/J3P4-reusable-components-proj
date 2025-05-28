f55a87c6693ea67aeca7353f8f237b733ed46147
Koen Polman <KoenPolman_@outlook.com>
2025-05-28 15:45:19 +0200

    fix: refactored command interperter and unit behavior

--- GnomeOnslaught/Assets/Scripts/CommandInterperter.cs
+++ GnomeOnslaught/Assets/Scripts/CommandInterperter.cs
@@ -1,31 +1,58 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class CommandInterperter : MonoBehaviour
 {
     private UnitBehavior unitBehavior;
+    private Rigidbody2D rigidbody2D;
     private Vector2 movementDirection;
+    private UnitBehaviorType currentUnitBehavior = UnitBehaviorType.idle;
 
     void Start()
     {
+        rigidbody2D = GetComponent<Rigidbody2D>();
         unitBehavior = GetComponent<UnitBehavior>();
     }
     void Update()
     {
+        switch (currentUnitBehavior)
+        {
+            case UnitBehaviorType.idle:
+                movementDirection = Vector2.zero;
+                break;
+            case UnitBehaviorType.attacking:
+                AttackTarget(unitBehavior.target);
+                break;
+            case UnitBehaviorType.following:
+                FollowTarget(unitBehavior.target);
+                break;
+            default:
+                break;
+        }
 
-        unitBehavior.movementDirection = movementDirection;
+        GetComponent<Rigidbody2D>().AddForce(movementDirection);
     }
-
-    void followTarget(GameObject targetToFollow)
+    void AttackTarget(GameObject targetToFollow)
     {
-
+        Vector2 direction = (targetToFollow.transform.position - transform.position).normalized;
+        Debug.DrawLine(transform.position, targetToFollow.transform.position, Color.red);
+        Debug.Log("Direction: " + direction);
     }
 
     void GoToDirection()
     {
 
     }
-    void SeekEnemy()
-    {
 
+    void FollowTarget(GameObject targetToFollow)
+    {
+        Vector2 toTarget = targetToFollow.transform.position - transform.position;
+        float currentDistance = toTarget.magnitude;
+        // Only move if we're farther than the desired distance
+        if (currentDistance > 1)
+        {
+            Vector2 direction = toTarget.normalized;
+            rigidbody2D.AddForce(direction);
+        }
     }
 }
--- GnomeOnslaught/Assets/Scripts/UnitBehavior.cs
+++ GnomeOnslaught/Assets/Scripts/UnitBehavior.cs
@@ -1,59 +1,25 @@
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class UnitBehavior : MonoBehaviour
 {
     [SerializeField] float movementSpeed;
     [SerializeField] FactionTypes faction;
-    private UnitBehaviorType currentUnitBehavior = UnitBehaviorType.idle;
-    private GameObject target;
-    public Vector2 movementDirection;
-    private Rigidbody2D rigidbody2D;
+    public GameObject target
+    { get; set; }
 
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+
     }
 
     void Update()
     {
-        switch(currentUnitBehavior)
-        {
-            case UnitBehaviorType.idle:
-                break;
-            case UnitBehaviorType.attacking:
-                AttackTarget(target);
-                break;
-            case UnitBehaviorType.following:
-                break;
-                FollowTarget(target); break;
-            default;
-                break;
-        }
-
-        rigidbody2D.AddForce(movementDirection);
-    }
-    void AttackTarget(GameObject targetToFollow)
-    {
-        Vector2 direction = (targetToFollow.transform.position - transform.position).normalized;
-        Debug.DrawLine(transform.position, targetToFollow.transform.position, Color.red);
-        Debug.Log("Direction: " + direction);
-    }
-
-    void GoToDirection()
-    {
 
     }
+    
 
-    void FollowTarget(GameObject targetToFollow)
-    {
-        Vector2 toTarget = targetToFollow.transform.position - transform.position;
-        float currentDistance = toTarget.magnitude;
-
-        // Only move if we're farther than the desired distance
-        if (currentDistance > 1)
-        {
-            Vector2 direction = toTarget.normalized;
-            rigidbody2D.AddForce(direction);
-        }
-    }
 }
+
+
